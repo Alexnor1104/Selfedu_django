84b4661d0c56f33b621af10609ed1b58c8041d3e
Alexnor11 <advance114@gmail.com>
2022-01-03 21:43:06 +0300

    my commit

---
+++ coolsite/women/views.py
@@ -0,0 +1,70 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import AddPostForm
+from .models import *
+
+# Create your views here.
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
+
+
+def index(request):
+    posts = Women.objects.all()
+    context = {'posts': posts,
+               'menu': menu,
+               'title': 'Главная страница',
+               'cat_selected': 0,
+               }
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            form.save()
+            return redirect('home')
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'menu': menu, 'title': 'Добавление статьи', 'form': form})
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'women/post.html', context=context)
+
+
+def show_category(request, cat_slug):
+    posts = Women.objects.filter(slug=cat_slug)
+
+    context = {'posts': posts,
+               'menu': menu,
+               'title': 'Главная страница',
+               'cat_selected': cat_slug,
+               }
+    return render(request, 'women/index.html', context=context)
